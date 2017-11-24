55374a6b386d4e928824c2d68fdf792a69f5bfda
Florian B <floryn93@gmail.com>
2017-11-24 20:29:02 +0200

    Add color detector project.

---
+++ ColorDetector/README.md
@@ -0,0 +1,25 @@
+# Color detector
+
+## Requirements
+
+* [Microsoft Visual Studio](https://https://www.visualstudio.com/)
+* [OpenCV](https://opencv.org/)
+* [Qt](https://www.qt.io/)
+
+## Installation
+
+* Download the project.
+* Create `Images1` and `Images2` directories in `D:\` and add 3 images in each one.
+* Open it with `Qt`.
+* In the file with `.pro` extension set `INCLUDEPATH` and `LIBS`.
+* Build the project and then run it.
+
+## Graphical user interface
+
+* Initial image
+
+![color detector v1 n](https://user-images.githubusercontent.com/20202617/33221040-72f7995a-d155-11e7-949e-b550086d5f9f.png)
+
+* Processed image
+
+![color detector v1 m](https://user-images.githubusercontent.com/20202617/33221052-81a406dc-d155-11e7-995d-be95e32c814b.png)
---
+++ ColorDetector/mainwindow.h
@@ -0,0 +1,55 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFileDialog>
+#include <QtGui>
+#include <QMessageBox>
+#include <QColorDialog>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include "colordetector.h"
+
+using namespace cv;
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_chooseFileBtn_triggered();
+
+    void on_chooseColorBtn_triggered();
+
+    void on_processImageBtn_triggered();
+
+    void on_saveImageBtn_triggered();
+
+    void on_setColorDistanceSlider_valueChanged(int value);
+
+    void on_manhattanRadioBtn_toggled(bool checked);
+
+    void on_euclideanRadioBtn_toggled(bool checked);
+
+    void on_chebyshevRadioBtn_toggled(bool checked);
+
+    void on_minkowskiRadioBtn_toggled(bool checked);
+
+private:
+    Ui::MainWindow *ui;
+    QMessageBox errorBox;
+    Mat image;
+    Mat result;
+    ColorDetector cDetect;
+};
+
+#endif // MAINWINDOW_H
